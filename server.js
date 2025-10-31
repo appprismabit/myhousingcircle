25ec295d69b0b84715282f14662b46e429a70e02
appprismabit <app.prismabit@gmail.com>
2025-10-31 20:26:20 +0530

    Add mongo

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import redisClient from './src/config/redisClient.js';
+import morgan from 'morgan';
+import { connectDB } from './src/config/database.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 const cache = async (req, res, next) => {
   const { id } = req.params;
@@ -18,7 +26,6 @@ const cache = async (req, res, next) => {
   next();
 };
 
-// Example route
 app.get("/user/:id", cache, async (req, res) => {
   const { id } = req.params;
 
@@ -31,6 +38,25 @@ app.get("/user/:id", cache, async (req, res) => {
   res.json(user);
 });
 
+app.get('/users', async (req, res) => {
+  try {
+    const db = await connectDB();
+    const query = await db.collection("users").findOne({
+      phone: "9129922459"
+    });
+
+    return res.status(200).json({
+      message: "User data fetched successfully!",
+      data: query
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Some error occured!",
+      error: e.message
+    });
+  }
+
+});
 
 
 app.get('/test', (req, res) => {
---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectDB = async () => {
+  if (isConnected) {
+    console.log("🔁 Reusing existing MongoDB connection");
+    return mongoose.connection;
+  }
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+      maxPoolSize: 10
+    });
+
+    isConnected = true;
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    return conn;
+  } catch (error) {
+    console.log("❌ MongoDB Connection Error:", error.message);
+    process.exit(1);
+  }
+};
