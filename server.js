f742d64df3283b73c7863f8559a19ccac6c7f8ef
appprismabit <app.prismabit@gmail.com>
2025-10-31 12:14:46 +0530

    Workflow test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/test', (req, res) => {
-  res.send('Hello, World CI/CD!');
+  return res.status(200).json({ message: 'Server is running!' });
 });
 
 app.listen(PORT, () => {
