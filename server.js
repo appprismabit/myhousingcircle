3ceb80a97cab059904622224ed204549242437af
appprismabit <app.prismabit@gmail.com>
2025-10-31 12:57:45 +0530

    fix: Change PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.get('/test', (req, res) => {
   return res.status(200).json({ message: 'Server is running!' });
